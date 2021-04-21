a984f2d13eeaf1abf7ab51f63ca3e2440cd53fc3
Mykhailo <vatazkomisa@gmail.com>
2021-04-21 23:23:11 +0300

    training2_v2

--- ЛБ2++/main.cpp
+++ ЛБ2++/main.cpp
@@ -5,5 +5,21 @@ int main()
 {
 	int t, z;
 	cout << "t = "; cin >> t;
-
+	z = t % 6;
+	if ((z == 1) || (z == 2) || (z == 3))
+	{
+		cout << "Green light\n";
+	}
+	else
+	{
+		if (z == 4)
+		{
+			cout << "Yellow light\n";
+		}
+		else
+		{
+			cout << "Red light\n";
+		}
+	}
+	system("pause");
 }
